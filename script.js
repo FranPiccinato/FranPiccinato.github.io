9dd3a50c17b85a27c3d6c9a897594f2ba1b6f277
FranPiccinato <107905840+FranPiccinato@users.noreply.github.com>
2024-05-03 10:39:24 -0600

    Checked button changes

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 "use strict"
+
+var todoList = document.querySelectorAll('.btn-check');
+todoList.forEach(todo => {
+    todo.addEventListener('click', () =>{
+        todo.classList.toggle('checked');
+    });
+});
+
+
