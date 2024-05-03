940a0b190a9edd90aec4398f03b669c88df97011
FranPiccinato <107905840+FranPiccinato@users.noreply.github.com>
2024-05-03 12:43:16 -0600

    Buttons

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 "use strict"
 
 var todoList = document.querySelectorAll('.btn-check');
+
 todoList.forEach(todo => {
     todo.addEventListener('click', () =>{
         todo.classList.toggle('checked');
@@ -8,3 +9,16 @@ todoList.forEach(todo => {
 });
 
 
+var buttonState = document.querySelector('#btn-state');
+var icon = document.querySelector('#img-state');
+
+
+function modeChange(){
+    if(icon.src == 'images/icon-moon.svg'){
+        icon.src = 'images/icon-sun.svg';
+    }else{
+        icon.src = 'images/icon-moon.svg';
+    }
+}
+
+buttonState.addEventListener('click', modeChange);
