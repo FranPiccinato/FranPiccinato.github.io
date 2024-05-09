d8139c531b652e6bd08bd76cdf346d9a5196a03f
FranPiccinato <107905840+FranPiccinato@users.noreply.github.com>
2024-05-09 10:45:55 -0600

    cookies

--- script.js
+++ script.js
@@ -1,4 +1,32 @@
 "use strict"
+
+function setCookie(cname, value, days){
+    const date = new Date();
+    date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+    let expires = "expires=" + date.toUTCString();
+    document.cookie = `${cname}=${value}; ${expires}; path=/`
+
+}
+
+function deleteCookie(cname){
+    setCookie(cname, null, null);
+}
+
+function getCookie(cname){
+    const cDecoded = decodeURIComponent(document.cookie);
+    const cArray = cDecoded.split("; ");
+    let result = null;
+
+    cArray.forEach(e =>{
+        if(e.indexOf(cname) == 0){
+            result = e.substring(cname.length + 1);
+        }
+    })
+
+    return result;
+}
+
+setTheme(getCookie("theme"));
 var nItems = document.querySelector("#nItems");
 var count = 0;
 
@@ -19,7 +47,7 @@ checked.addEventListener('click', () => {
 
 var buttonState = document.querySelector('#btn-state');
 
-function modeChange() {
+function setTheme(themeName) {
     var icon = document.querySelector('#img-state');
     var body = document.body;
     var containerNew = document.querySelector('#containerNewTodo');
@@ -33,11 +61,9 @@ function modeChange() {
     var btnCom = document.querySelector("#btn-completed");
     var btnCl = document.querySelector("#btn-clear");
 
-    const img = icon.src;
-    const imageName = img.match(/\w+(?=\.svg$)/)[0];
     var btnCh = document.querySelectorAll('.btn-check');
 
-    if (imageName == 'moon') {
+    if (themeName == 'moon') {
         icon.src = 'images/icon-sun.svg';
 
         body.classList.add("bodyDark");
@@ -76,6 +102,107 @@ function modeChange() {
         btnCl.classList.add("buttons-dark");
         btnCl.classList.remove("buttons-light");
     } else {
+        icon.src = 'images/icon-moon.svg';
+        body.classList.remove("bodyDark");
+
+        containerNew.classList.remove("DarkNewTodo");
+        containerNew.classList.add("lightNewTodo");
+
+        btnCh.forEach(btn => {
+            btn.classList.add("btn-checkLight");
+            btn.classList.remove("btn-checkDark");
+        });
+
+        containerList.classList.add("containerListLight");
+        containerList.classList.remove("containerListDark");
+
+        containerFilter.classList.add("FilterLight");
+        containerFilter.classList.remove("FilterDark");
+
+        attri.classList.add("footerLight");
+        attri.classList.remove("footerDark");
+
+        li.forEach(item => {
+            item.classList.add("lightLine");
+            item.classList.remove("darkLine");
+        });
+
+        input.classList.add("inputLight");
+        input.classList.remove("inputDark");
+
+        btnAc.classList.add("buttons-light");
+        btnAc.classList.remove("buttons-dark");
+        btnAll.classList.add("buttons-light");
+        btnAll.classList.remove("buttons-dark");
+        btnCom.classList.add("buttons-light");
+        btnCom.classList.remove("buttons-dark");
+        btnCl.classList.add("buttons-light");
+        btnCl.classList.remove("buttons-dark");
+    }
+}
+
+function modeChange() {
+    var icon = document.querySelector('#img-state');
+    var body = document.body;
+    var containerNew = document.querySelector('#containerNewTodo');
+    var containerList = document.querySelector('#containerList');
+    var containerFilter = document.querySelector('#containerFilter');
+    var attri = document.querySelector(".attribution");
+    var li = document.querySelectorAll("li");
+    var input = document.querySelector("input");
+    var btnAc = document.querySelector("#btn-active");
+    var btnAll = document.querySelector("#btn-all");
+    var btnCom = document.querySelector("#btn-completed");
+    var btnCl = document.querySelector("#btn-clear");
+
+    const img = icon.src;
+    const imageName = img.match(/\w+(?=\.svg$)/)[0];
+    var btnCh = document.querySelectorAll('.btn-check');
+
+    if (imageName == 'moon') {
+        setCookie("theme", imageName , 365);
+        console.log(document.cookie);
+        icon.src = 'images/icon-sun.svg';
+
+        body.classList.add("bodyDark");
+
+        containerNew.classList.add("DarkNewTodo");
+        containerNew.classList.remove("lightNewTodo");
+
+        btnCh.forEach(btn => {
+            btn.classList.add("btn-checkDark");
+            btn.classList.remove("btn-checkLight");
+        });
+
+        containerList.classList.add("containerListDark");
+        containerList.classList.remove("containerListLight");
+
+        containerFilter.classList.add("FilterDark");
+        containerFilter.classList.remove("FilterLight");
+
+        attri.classList.add("footerDark");
+        attri.classList.remove("footerLight");
+
+        li.forEach(item => {
+            item.classList.add("darkLine");
+            item.classList.remove("lightLine");
+        });
+
+        input.classList.add("inputDark");
+        input.classList.remove("inputLight");
+
+        btnAc.classList.add("buttons-dark");
+        btnAc.classList.remove("buttons-light");
+        btnAll.classList.add("buttons-dark");
+        btnAll.classList.remove("buttons-light");
+        btnCom.classList.add("buttons-dark");
+        btnCom.classList.remove("buttons-light");
+        btnCl.classList.add("buttons-dark");
+        btnCl.classList.remove("buttons-light");
+    } else {
+        setCookie("theme", imageName , 365);
+        console.log(document.cookie);
+
         icon.src = 'images/icon-moon.svg';
         body.classList.remove("bodyDark");
 
